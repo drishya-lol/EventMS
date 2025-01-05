d4e8f65d778bb76b8808e342671039edb28d7458
drishyalol <sdrishyaraj@gmail.com>
2025-01-05 17:34:59 +0545

    Started working on Views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from models import User, Role, Event, EventCategory, EventRegistration, Vendor, VendorCategory, VendorPerformance, Ticket, Invoice, Logistics, Inventory, EventReview, VendorFeedback, Report
 
 # Create your views here.
